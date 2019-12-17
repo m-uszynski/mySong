c0bdb6a1fb5c15362d1b07ac19b8d4e1249b340e
Mateusz Uszyński <rtck@o2.pl>
2019-12-17 21:42:38 +0100

    Add server.js (REST API)

---
+++ server.js
@@ -0,0 +1,57 @@
+// Do uruchomienia: node -r esm server.js
+
+import express from 'express';
+import cors from 'cors';
+
+// API Configuration
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MySQL Configuration
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "remotemysql.com",
+    database: "qewTevNSN7",
+    user: "qewTevNSN7",
+    password: "3pNWfu5kSQ"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected with database!");
+})
+
+// API Functions
+
+app.get('/songs', function (req, res) {
+    // res.send("Hello world is working");
+    con.query("SELECT * FROM songs", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+    })
+});
+
+app.post('/songs', (req, res) => {
+    const song = req.body;
+
+    if (typeof song.ytlink !== 'undefined') {
+        var sqlQuery = "INSERT INTO songs (id,title,authors,text,ytlink) VALUES (NULL,'" + song.title + "','" + song.authors + "','" + song.text + "','" + song.ytlink + "'); ";
+    } else {
+        var sqlQuery = "INSERT INTO songs (id,title,authors,text,ytlink) VALUES (NULL,'" + song.title + "','" + song.authors + "','" + song.text + "',NULL); ";
+    }
+
+
+    con.query(sqlQuery, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+    res.send(song);
+});
+
+
+// Run API
+
+app.listen(8080, "82.139.172.100", () => {
+    console.log("App listening on port 8080");
+});
